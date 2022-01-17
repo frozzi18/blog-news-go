99008c67d7dfd063eedbac990f1c3111bec48082
frozzi18 <42233054+frozzi18@users.noreply.github.com>
2022-01-17 11:18:52 +0700

    update test

--- main.go
+++ main.go
@@ -1,1 +1,34 @@
+package main
 
+import (
+	"html/template"
+	"log"
+	"net/http"
+	"os"
+
+	"github.com/joho/godotenv"
+)
+
+var tpl = template
+
+func indexHandler(w http.ResponseWriter, r *http.Request) {
+	w.Write([]byte("<h1>Hello world!</h1>"))
+}
+
+func main() {
+	err := godotenv.Load()
+	if err != nil {
+		log.Println("Error loading .env file")
+	}
+
+	port := os.Getenv("PORT")
+	if port == "" {
+		log.Println(os.Getenv("PORT"))
+		port = "3000"
+	}
+
+	mux := http.NewServeMux()
+
+	mux.HandleFunc("/", indexHandler)
+	http.ListenAndServe(":"+port, mux)
+}
